10607f553930f68d2bf036e728f651a379301a28
sindre0830 <sindre0830@gmail.com>
2025-03-07 00:05:25 +0100

    feat: Add ReplayBuffer class for experience replay and update IGameAgent interface

--- src/sandbox_rl/application/game_agents.py
+++ src/sandbox_rl/application/game_agents.py
@@ -1,11 +1,21 @@
 import sandbox_rl.core.interfaces
 import numpy as np
 import interface
+import typing
 
 
 class RandomAgent(interface.implements(sandbox_rl.core.interfaces.IGameAgent)):
     def __init__(self, seed: int = None) -> None:
         self.rng = np.random.default_rng(seed)
 
-    def select_action(self, game_state: sandbox_rl.core.interfaces.IGameState) -> np.ndarray:
-        return self.rng.choice(game_state.get_legal_actions())
+    def select_action(self, game_state: sandbox_rl.core.interfaces.IGameState) -> typing.Tuple[np.ndarray, float]:
+        legal_actions = game_state.get_legal_actions()
+        num_actions = len(legal_actions)
+        probabilities = np.ones(num_actions) / num_actions
+
+        value = 0.0
+
+        return probabilities, value
+
+    def train(self, batch: typing.Any) -> None:
+        pass
--- src/sandbox_rl/core/interfaces.py
+++ src/sandbox_rl/core/interfaces.py
@@ -25,7 +25,32 @@ class IGameState(interface.Interface):
     def encode_state(self) -> np.ndarray:
         raise NotImplementedError()
 
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
 
 class IGameAgent(interface.Interface):
-    def select_action(self, game_state: IGameState) -> np.ndarray:
+    def select_action(self, game_state: IGameState) -> typing.Tuple[np.ndarray, float]:
+        raise NotImplementedError()
+
+    def train(self, batch: typing.Any) -> None:
+        raise NotImplementedError()
+
+
+class ILearningAgent(interface.Interface):
+    def execute(self) -> None:
+        raise NotImplementedError()
+
+
+class IReplayBuffer(interface.Interface):
+    def __init__(self, max_size: int = 10000) -> None:
+        raise NotImplementedError()
+
+    def store(self, data: typing.Tuple) -> None:
+        raise NotImplementedError()
+
+    def sample(self, batch_size: int) -> typing.List[typing.Tuple]:
+        raise NotImplementedError()
+
+    def __len__(self) -> int:
         raise NotImplementedError()
